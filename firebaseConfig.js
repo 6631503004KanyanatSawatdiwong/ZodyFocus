32d532c3db4adb0707ee693f8c1598d9c721cf25
6631503004KanyanatSawatdiwong <157001522+6631503004KanyanatSawatdiwong@users.noreply.github.com>
2025-12-04 18:44:08 +0700

    🔒 Security: Move credentials to environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyBBEmvll6lP5V6-WpPWr_ZyLJFwfyJBSsA",
-    authDomain: "zodyfocus.firebaseapp.com",
-    databaseURL: "https://zodyfocus-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "zodyfocus",
-    storageBucket: "zodyfocus.firebasestorage.app",
-    messagingSenderId: "1003156842237",
-    appId: "1:1003156842237:android:947879f7f4ec57a1ca0094",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
